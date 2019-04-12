5bbd4298acc90e9cca510ad7f5b92a72685c5888
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-04-12 10:34:13 -0700

    bootstraping configuration and database connection

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,18 @@
 <?php
 
+// Uncomment the lines below to display errors before configuration has been loaded
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 define('PUBLIC_FILES', __DIR__);
 
-//ini_set('display_errors', 1);
-//error_reporting(E_ALL);
+// Define an autoloader for custom PHP classes so we don't have to
+// include their files manually
+spl_autoload_register(function ($className) {
+    include PUBLIC_FILES . '/lib/classes/' . str_replace('\\', '/', $className) . '.php';
+});
+
+// Load configuration
+$configManager = new Util\ConfigManager(PUBLIC_FILES . '/config' );
+
+$dbConn = DataAccess\DatabaseConnection::FromConfig($configManager->getDatabaseConfig());
