c3b4fc771874cc06e7531a2369f853dcede85edd
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-04-12 10:32:07 -0700

    defining user model classes

---
+++ lib/classes/Model/UserAuthProvider.php
@@ -0,0 +1,56 @@
+<?php
+namespace Model;
+
+class UserAuthProvider {
+
+    /** @var integer */
+    private $id;
+
+    /** @var string */
+    private $name;
+
+    public function __construct($id, $name) {
+        $this->setId($id);
+        $this->setName($name);
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
