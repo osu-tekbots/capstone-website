c29d43d3fa9c24a6855493c95aa908917370885d
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-04-18 10:18:52 -0700

    preparing to split shared libraries

--- bootstrap.php
+++ bootstrap.php
@@ -7,9 +7,21 @@ error_reporting(E_ALL);
 define('PUBLIC_FILES', __DIR__);
 
 // Define an autoloader for custom PHP classes so we don't have to
-// include their files manually
+// include their files manually. The autoloader should check for the
+// repository specific classes and shared classes.
 spl_autoload_register(function ($className) {
-    include PUBLIC_FILES . '/lib/classes/' . str_replace('\\', '/', $className) . '.php';
+    $phpFile = str_replace('\\', '/', $className) . '.php';
+    $local = PUBLIC_FILES . '/lib/classes/' . $phpFile;
+    $shared = PUBLIC_FILES . '/lib/shared/classes/' . $phpFile;
+    if(file_exists($local)) {
+        include $local;
+        return true;
+    }
+    if(file_exists($shared)) {
+        include $shared;
+        return true;
+    }
+    return false;
 });
 
 // Load configuration
