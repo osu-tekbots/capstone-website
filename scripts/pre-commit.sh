25cb81bb14b7b45e0529b1275d18fdf2a3c3ad54
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-04-18 17:49:43 -0700

    updating pre-commit docs

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -2,10 +2,8 @@
 #
 # This script enforces some rules before changes are committed. For now, it just
 # sets the file permissions for all files and directories so they are accessible
-# from the browser. To use it, execute the following in Linux or bash, or just
-# copy the file as `pre-commit` in the .git/hooks directory.
-#
-# ln -s scripts/pre-commit.sh .git/hooks/pre-commit
+# from the browser. Copy this file as `pre-commit` into `.git/hooks` and make it
+# executable.
 #
 echo "Starting pre-commit..."
 
