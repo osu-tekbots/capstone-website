1d59920fe1693087678077ddb9c4d90b45c05f6f
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-04-22 10:44:54 -0700

    adding status code for NOT_FOUND

--- lib/shared/classes/Api/Response.php
+++ lib/shared/classes/Api/Response.php
@@ -9,6 +9,7 @@ class Response {
     public const CREATED = 201;
     public const BAD_REQUEST = 400;
     public const UNAUTHORIZED = 401;
+    public const NOT_FOUND = 404;
     public const INTERNAL_SERVER_ERROR = 500;
 
     /** @var integer */
