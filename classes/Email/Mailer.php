d09b79dfcf618de76bc8401c7265519cc1bd342c
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-05-09 11:23:59 -0700

    Squashed 'lib/shared/' changes from 685ad48..f708059
    f708059 adding debugging, better HTML support
    150e47a adding HTML email capabilities
    1eab68f implementing generic ONID login provider
    
    git-subtree-dir: lib/shared
    git-subtree-split: f708059d077de82680aa2ab1a4327e3c1f43672a

--- classes/Email/Mailer.php
+++ classes/Email/Mailer.php
@@ -13,15 +13,20 @@ class Mailer {
     /** @var string */
     private $subjectTag;
 
+    /** @var \Util\Logger */
+    protected $logger;
+
     /**
      * Creates a new mailer to send emails.
      *
      * @param string $from the from address for the email
      * @param string|null $subjectTag an optional tag to prefix the email subject with
+     * @param \Util\Logger|null $logger an optional logger to capture error messages from the mail() function
      */
-    public function __construct($from, $subjectTag = null) {
+    public function __construct($from, $subjectTag = null, $logger = null) {
         $this->from = $from;
         $this->subjectTag = $subjectTag;
+        $this->logger = $logger;
     }
 
     /**
@@ -30,27 +35,48 @@ class Mailer {
      * @param string|string[] $to the email address or addresses to send the message to
      * @param string $subject the subject of the email
      * @param string $message the email content to send
+     * @param boolean $html indicates whether the message content is HTML or plain text
      * @return boolean true on success, false otherwise
      */
-    public function sendEmail($to, $subject, $message) {
+    public function sendEmail($to, $subject, $message, $html = false) {
         if ($this->subjectTag != null) {
-            $subject = $this->subjectTag . ' ' . $subject;
+            $subject = '[' . $this->subjectTag . '] ' . $subject;
         }
 
         $from = $this->from;
 
-        $headers = array(
-            "From: $from"
-        );
+        $headers = array();
+
+        if ($html) {
+            $message = "
+            <html>
+            <head> 
+                <title>$subject</title>
+            </head>
+            <body>
+                $message
+            </body>
+            </html>
+            ";
+
+            $headers[] = 'MIME-Version: 1.0';
+            $headers[] = 'Content-Type: text/html;charset=UTF-8';
+        }
+
+        $headers[] = "From: $from";
 
         $headersStr = \implode('\r\n', $headers);
         
-        if(\is_array($to)) {  
+        if (\is_array($to)) {
             $to = \implode(',', $to);
         }
 
         $accepted = \mail($to, $subject, $message, $headersStr);
         if (!$accepted) {
+            if ($this->logger != null) {
+                $this->logger->error("Failed to send email to $to from $from: " . error_get_last()['message']);
+                $this->logger->error($message);
+            }
             return false;
         }
 
