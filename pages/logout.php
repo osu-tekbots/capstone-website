8c552ed7e17d6bdd957317f7b3d670c4e43e8a3d
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-05-09 12:41:54 -0700

    making session unset more specific

--- pages/logout.php
+++ pages/logout.php
@@ -1,5 +1,8 @@
 <?php 
     session_start();
+    unset($_SESSION['userID']);
+    unset($_SESSION['accessLevel']);
+    unset($_SESSION['newUser']);
     session_unset();
     session_destroy();
     header('Location: ./index.php');
