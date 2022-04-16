1111c0dfc6517eb743b9eba17546c4003ff8dda5
DotBowder <d.bowder@gmail.com>
2022-04-16 10:29:17 -0700

    Add teardown steps for user_local_auth

--- src/public/scripts/database/teardown-user.sql
+++ src/public/scripts/database/teardown-user.sql
@@ -2,4 +2,6 @@ DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS user_type;
 DROP TABLE IF EXISTS user_salutation;
 DROP TABLE IF EXISTS user_auth_provider;
+DROP TABLE IF EXISTS user_local_auth;
+DROP TABLE IF EXISTS user_local_auth_salt;
  
