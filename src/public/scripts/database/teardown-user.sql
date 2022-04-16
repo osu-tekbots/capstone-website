3a835d140f3751e4c50c8f8c5ce0d30eedb31e8e
DotBowder <d.bowder@gmail.com>
2022-04-16 10:33:12 -0700

    Change order of user teardown

--- src/public/scripts/database/teardown-user.sql
+++ src/public/scripts/database/teardown-user.sql
@@ -1,7 +1,7 @@
+DROP TABLE IF EXISTS user_local_auth_salt;
+DROP TABLE IF EXISTS user_local_auth;
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS user_type;
 DROP TABLE IF EXISTS user_salutation;
 DROP TABLE IF EXISTS user_auth_provider;
-DROP TABLE IF EXISTS user_local_auth;
-DROP TABLE IF EXISTS user_local_auth_salt;
  
