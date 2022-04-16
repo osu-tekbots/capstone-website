55c217e4b1f90d2398e01c2b8727717965de8c00
DotBowder <d.bowder@gmail.com>
2022-04-16 10:42:17 -0700

    Disable FK checks when teardown

--- src/public/scripts/database/teardown-capstone-application.sql
+++ src/public/scripts/database/teardown-capstone-application.sql
@@ -1,4 +1,6 @@
+SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS capstone_application_review;
 DROP TABLE IF EXISTS capstone_application;
 DROP TABLE IF EXISTS capstone_application_status;
 DROP TABLE IF EXISTS capstone_interest_level;
+SET FOREIGN_KEY_CHECKS = 1;
--- src/public/scripts/database/teardown-user.sql
+++ src/public/scripts/database/teardown-user.sql
@@ -1,6 +1,8 @@
+SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS user_local_auth_salt;
 DROP TABLE IF EXISTS user_local_auth;
 DROP TABLE IF EXISTS user_type;
 DROP TABLE IF EXISTS user_salutation;
 DROP TABLE IF EXISTS user_auth_provider;
 DROP TABLE IF EXISTS user;
+SET FOREIGN_KEY_CHECKS = 1;
