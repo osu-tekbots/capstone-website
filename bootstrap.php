b2c823754455bf2bee4a7fb535e20d209fa1a4ee
radiantBear <85893453+radiantBear@users.noreply.github.com>
2023-12-08 16:12:17 -0800

    Protect DB credentials in case php error display is on (err msg shows credentials)

--- bootstrap.php
+++ bootstrap.php
@@ -11,7 +11,12 @@ include PUBLIC_FILES . '/lib/shared/autoload.php';
 // Load configuration
 $configManager = new Util\ConfigManager(PUBLIC_FILES);
 
-$dbConn = DataAccess\DatabaseConnection::FromConfig($configManager->getDatabaseConfig());
+try {
+    $dbConn = DataAccess\DatabaseConnection::FromConfig($configManager->getDatabaseConfig());
+} catch (\Exception $e) {
+    echo 'There is an irresolvable issue with our database connection right now. Please try again later.';
+    die();
+}
 
 try {
     $logger = new Util\Logger($configManager->getLogFilePath(), $configManager->getLogLevel());
